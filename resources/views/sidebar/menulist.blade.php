b75af3635a9a6901a90ad7d96ea4f1233bcfe62d
Tanvir <aiub.tanvir@gmail.com>
2024-07-31 17:22:53 +0600

    interview chnage status
    Signed-off-by: Tanvir <aiub.tanvir@gmail.com>

--- resources/views/sidebar/menulist.blade.php
+++ resources/views/sidebar/menulist.blade.php
@@ -188,7 +188,7 @@
         </li>
         @endif
         @if(Auth::check() && Auth::user()->role=='adminManager' || Auth::user()->role=='admin' || Auth::user()->role=='manager' || Auth::user()->role=='interviewer')
-        <li class="menu {{ (!empty($attend) && $attend==true)?'active':'' }}">
+        <!--<li class="menu {{ (!empty($attend) && $attend==true)?'active':'' }}">
             <a href="#datatables10" data-bs-toggle="collapse" aria-expanded="{{ (!empty($attend) && $attend==true)?'true':'false' }}" class="dropdown-toggle">
                 <div class="">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book-open"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>
@@ -223,7 +223,7 @@
                 </li>
                 @endif
             </ul>
-        </li>
+        </li>-->
         @endif
         @if(Auth::check() && Auth::user()->role=='agent' && Auth::user()->is_admin==1)
         <li class="menu {{ (!empty($agent_user) && $agent_user==true)?'active':'' }}">
